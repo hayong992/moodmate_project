47a56b24f05468f14cee2f16b5127e51d41b0bf1
hayong992 <dydcodud123@naver.com>
2024-11-25 19:47:02 +0900

    Update python app.py

--- .github/workflows/python app.py
+++ .github/workflows/python app.py
@@ -1,30 +1,29 @@
-name: Python CI
+name: Python application
 
 on:
   push:
-    branches:
-      - main
+    branches: [ main ]
   pull_request:
-    branches:
-      - main
+    branches: [ main ]
 
 jobs:
   build:
     runs-on: ubuntu-latest
 
     steps:
-    - name: Checkout code
-      uses: actions/checkout@v2
+      - name: Checkout code
+        uses: actions/checkout@v2
 
-    - name: Set up Python
-      uses: actions/setup-python@v2
-      with:
-        python-version: '3.9'
+      - name: Set up Python
+        uses: actions/setup-python@v2
+        with:
+          python-version: '3.x'
 
-    - name: Install dependencies
-      run: |
-        python -m pip install --upgrade pip
-        pip install -r requirements.txt
+      - name: Install dependencies
+        run: |
+          python -m pip install --upgrade pip
+          pip install -r requirements.txt
 
-    - name: Run Python script
-      run: python app.py  # app.py 파일을 실행하도록 수정
+      - name: Run script
+        run: |
+          python app.py  # 수정된 부분
